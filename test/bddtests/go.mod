fc1270cb7617bc7d744e2942d0382e5563a3e893
Sandra Vrtikapa <sandra.vrtikapa@securekey.com>
2019-06-17 16:09:53 -0400

    test: Sidetree Txn Integration Test
    Closes #25
    
    Signed-off-by: Sandra Vrtikapa <sandra.vrtikapa@securekey.com>

--- test/bddtests/go.mod
+++ test/bddtests/go.mod
@@ -6,16 +6,13 @@ module github.com/trustbloc/sidetree-fabric/test/bddtests
 
 require (
 	github.com/DATA-DOG/godog v0.7.13
-	github.com/grpc-ecosystem/go-grpc-middleware v1.0.0 // indirect
-	github.com/hyperledger/fabric-amcl v0.0.0-20181230093703-5ccba6eab8d6 // indirect
-	github.com/hyperledger/fabric-sdk-go v1.0.0-alpha5.0.20190429134815-48bb0d199e2c
 	github.com/hyperledger/fabric-sdk-go/third_party/github.com/hyperledger/fabric v0.0.0-20190429134815-48bb0d199e2c
-	github.com/op/go-logging v0.0.0-20160315200505-970db520ece7 // indirect
 	github.com/pkg/errors v0.8.1
+	github.com/sirupsen/logrus v1.3.0 // indirect
 	github.com/spf13/viper v1.0.2
-	github.com/sykesm/zap-logfmt v0.0.2 // indirect
 	github.com/trustbloc/fabric-peer-test-common v0.0.0-20190528215613-a7959c5ba3e1
-	go.uber.org/zap v1.10.0 // indirect
+	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2 // indirect
+	golang.org/x/sys v0.0.0-20190219092855-153ac476189d // indirect
 
 )
 
---
+++ test/bddtests/offledger_steps.go
@@ -0,0 +1,140 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package bddtests
+
+import (
+	"crypto"
+	"encoding/base64"
+
+	"github.com/DATA-DOG/godog"
+	"github.com/hyperledger/fabric-sdk-go/third_party/github.com/hyperledger/fabric/protos/common"
+	"github.com/pkg/errors"
+	"github.com/trustbloc/fabric-peer-test-common/bddtests"
+)
+
+// OffLedgerSteps ...
+type OffLedgerSteps struct {
+	BDDContext *bddtests.BDDContext
+	content    string
+	address    string
+}
+
+// NewOffLedgerSteps ...
+func NewOffLedgerSteps(context *bddtests.BDDContext) *OffLedgerSteps {
+	return &OffLedgerSteps{BDDContext: context}
+}
+
+// DefineOffLedgerCollectionConfig defines a new off-ledger data collection configuration
+func (d *OffLedgerSteps) DefineOffLedgerCollectionConfig(id, name, policy string, requiredPeerCount, maxPeerCount int32, timeToLive string) {
+	d.BDDContext.DefineCollectionConfig(id,
+		func(channelID string) (*common.CollectionConfig, error) {
+			sigPolicy, err := d.newChaincodePolicy(policy, channelID)
+			if err != nil {
+				return nil, errors.Wrapf(err, "error creating collection policy for collection [%s]", name)
+			}
+			return newOffLedgerCollectionConfig(name, requiredPeerCount, maxPeerCount, timeToLive, sigPolicy), nil
+		},
+	)
+}
+
+// DefineDCASCollectionConfig defines a new DCAS collection configuration
+func (d *OffLedgerSteps) DefineDCASCollectionConfig(id, name, policy string, requiredPeerCount, maxPeerCount int32, timeToLive string) {
+	d.BDDContext.DefineCollectionConfig(id,
+		func(channelID string) (*common.CollectionConfig, error) {
+			sigPolicy, err := d.newChaincodePolicy(policy, channelID)
+			if err != nil {
+				return nil, errors.Wrapf(err, "error creating collection policy for collection [%s]", name)
+			}
+			return newDCASCollectionConfig(name, requiredPeerCount, maxPeerCount, timeToLive, sigPolicy), nil
+		},
+	)
+}
+
+func (d *OffLedgerSteps) setCASVariable(varName, value string) error {
+	casKey := GetCASKey([]byte(value))
+	bddtests.SetVar(varName, casKey)
+	return nil
+}
+
+// getHash will compute the hash for the supplied bytes using SHA256
+func getHash(bytes []byte) []byte {
+	h := crypto.SHA256.New()
+	// added no lint directive because there's no error from source code
+	// error cannot be produced, checked google source
+	h.Write(bytes) //nolint
+	return h.Sum(nil)
+}
+
+// GetCASKey returns the content-addressable key for the given content
+// (sha256 hash + base64 URL encoding).
+func GetCASKey(content []byte) string {
+	hash := getHash(content)
+	buf := make([]byte, base64.URLEncoding.EncodedLen(len(hash)))
+	base64.URLEncoding.Encode(buf, hash)
+	return string(buf)
+}
+
+func (d *OffLedgerSteps) defineOffLedgerCollectionConfig(id, collection, policy string, requiredPeerCount int, maxPeerCount int, timeToLive string) error {
+	d.DefineOffLedgerCollectionConfig(id, collection, policy, int32(requiredPeerCount), int32(maxPeerCount), timeToLive)
+	return nil
+}
+
+func (d *OffLedgerSteps) defineDCASCollectionConfig(id, collection, policy string, requiredPeerCount int, maxPeerCount int, timeToLive string) error {
+	d.DefineDCASCollectionConfig(id, collection, policy, int32(requiredPeerCount), int32(maxPeerCount), timeToLive)
+	return nil
+}
+
+func (d *OffLedgerSteps) newChaincodePolicy(ccPolicy, channelID string) (*common.SignaturePolicyEnvelope, error) {
+	return bddtests.NewChaincodePolicy(d.BDDContext, ccPolicy, channelID)
+}
+
+func newOffLedgerCollectionConfig(collName string, requiredPeerCount, maxPeerCount int32, timeToLive string, policy *common.SignaturePolicyEnvelope) *common.CollectionConfig {
+	return &common.CollectionConfig{
+		Payload: &common.CollectionConfig_StaticCollectionConfig{
+			StaticCollectionConfig: &common.StaticCollectionConfig{
+				Name:              collName,
+				Type:              common.CollectionType_COL_OFFLEDGER,
+				RequiredPeerCount: requiredPeerCount,
+				MaximumPeerCount:  maxPeerCount,
+				TimeToLive:        timeToLive,
+				MemberOrgsPolicy: &common.CollectionPolicyConfig{
+					Payload: &common.CollectionPolicyConfig_SignaturePolicy{
+						SignaturePolicy: policy,
+					},
+				},
+			},
+		},
+	}
+}
+
+func newDCASCollectionConfig(collName string, requiredPeerCount, maxPeerCount int32, timeToLive string, policy *common.SignaturePolicyEnvelope) *common.CollectionConfig {
+	return &common.CollectionConfig{
+		Payload: &common.CollectionConfig_StaticCollectionConfig{
+			StaticCollectionConfig: &common.StaticCollectionConfig{
+				Name:              collName,
+				Type:              common.CollectionType_COL_DCAS,
+				RequiredPeerCount: requiredPeerCount,
+				MaximumPeerCount:  maxPeerCount,
+				TimeToLive:        timeToLive,
+				MemberOrgsPolicy: &common.CollectionPolicyConfig{
+					Payload: &common.CollectionPolicyConfig_SignaturePolicy{
+						SignaturePolicy: policy,
+					},
+				},
+			},
+		},
+	}
+}
+
+// RegisterSteps registers off-ledger steps
+func (d *OffLedgerSteps) RegisterSteps(s *godog.Suite) {
+	s.BeforeScenario(d.BDDContext.BeforeScenario)
+	s.AfterScenario(d.BDDContext.AfterScenario)
+	s.Step(`^variable "([^"]*)" is assigned the CAS key of value "([^"]*)"$`, d.setCASVariable)
+	s.Step(`^off-ledger collection config "([^"]*)" is defined for collection "([^"]*)" as policy="([^"]*)", requiredPeerCount=(\d+), maxPeerCount=(\d+), and timeToLive=([^"]*)$`, d.defineOffLedgerCollectionConfig)
+	s.Step(`^DCAS collection config "([^"]*)" is defined for collection "([^"]*)" as policy="([^"]*)", requiredPeerCount=(\d+), maxPeerCount=(\d+), and timeToLive=([^"]*)$`, d.defineDCASCollectionConfig)
+
+}
