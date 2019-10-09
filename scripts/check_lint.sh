72c119a2c585c0c71e09e31017473c9eef020b84
Bob Stasyszyn <Bob.Stasyszyn@securekey.com>
2019-10-09 09:34:22 -0400

    chore: Use golangci-lint v1.20
    Re-enabled check_lint with golanci-lint v1.20 which uses much less memory.
    
    closes #62
    
    Signed-off-by: Bob Stasyszyn <Bob.Stasyszyn@securekey.com>

--- scripts/check_lint.sh
+++ scripts/check_lint.sh
@@ -11,4 +11,4 @@ echo "Running $0"
 
 DOCKER_CMD=docker
 
-${DOCKER_CMD} run --rm -e GOPROXY=${GOPROXY} -v $(pwd):/opt/workspace -w /opt/workspace golangci/golangci-lint golangci-lint run
+${DOCKER_CMD} run --rm -e GOPROXY=${GOPROXY} -v $(pwd):/opt/workspace -w /opt/workspace golangci/golangci-lint:v1.20 golangci-lint run
