2cff49c0e6ea288a56146c2eee4a7c6b49b0f2a4
Sandra Vrtikapa <sandra.vrtikapa@securekey.com>
2020-02-07 17:25:30 -0500

    chore: update to latest sidetree-core
    Update to latest sidetree-core
    
    Closes #119
    
    Signed-off-by: Sandra Vrtikapa <sandra.vrtikapa@securekey.com>

--- pkg/peer/mocks/batchwriter.gen.go
+++ pkg/peer/mocks/batchwriter.gen.go
@@ -6,10 +6,11 @@ import (
 )
 
 type BatchWriter struct {
-	AddStub        func(operation []byte) error
+	AddStub        func([]byte, string) error
 	addMutex       sync.RWMutex
 	addArgsForCall []struct {
-		operation []byte
+		arg1 []byte
+		arg2 string
 	}
 	addReturns struct {
 		result1 error
@@ -19,40 +20,44 @@ type BatchWriter struct {
 	}
 	StartStub        func() error
 	startMutex       sync.RWMutex
-	startArgsForCall []struct{}
-	startReturns     struct {
+	startArgsForCall []struct {
+	}
+	startReturns struct {
 		result1 error
 	}
 	startReturnsOnCall map[int]struct {
 		result1 error
 	}
-	StopStub         func()
-	stopMutex        sync.RWMutex
-	stopArgsForCall  []struct{}
+	StopStub        func()
+	stopMutex       sync.RWMutex
+	stopArgsForCall []struct {
+	}
 	invocations      map[string][][]interface{}
 	invocationsMutex sync.RWMutex
 }
 
-func (fake *BatchWriter) Add(operation []byte) error {
-	var operationCopy []byte
-	if operation != nil {
-		operationCopy = make([]byte, len(operation))
-		copy(operationCopy, operation)
+func (fake *BatchWriter) Add(arg1 []byte, arg2 string) error {
+	var arg1Copy []byte
+	if arg1 != nil {
+		arg1Copy = make([]byte, len(arg1))
+		copy(arg1Copy, arg1)
 	}
 	fake.addMutex.Lock()
 	ret, specificReturn := fake.addReturnsOnCall[len(fake.addArgsForCall)]
 	fake.addArgsForCall = append(fake.addArgsForCall, struct {
-		operation []byte
-	}{operationCopy})
-	fake.recordInvocation("Add", []interface{}{operationCopy})
+		arg1 []byte
+		arg2 string
+	}{arg1Copy, arg2})
+	fake.recordInvocation("Add", []interface{}{arg1Copy, arg2})
 	fake.addMutex.Unlock()
 	if fake.AddStub != nil {
-		return fake.AddStub(operation)
+		return fake.AddStub(arg1, arg2)
 	}
 	if specificReturn {
 		return ret.result1
 	}
-	return fake.addReturns.result1
+	fakeReturns := fake.addReturns
+	return fakeReturns.result1
 }
 
 func (fake *BatchWriter) AddCallCount() int {
@@ -61,13 +66,22 @@ func (fake *BatchWriter) AddCallCount() int {
 	return len(fake.addArgsForCall)
 }
 
-func (fake *BatchWriter) AddArgsForCall(i int) []byte {
+func (fake *BatchWriter) AddCalls(stub func([]byte, string) error) {
+	fake.addMutex.Lock()
+	defer fake.addMutex.Unlock()
+	fake.AddStub = stub
+}
+
+func (fake *BatchWriter) AddArgsForCall(i int) ([]byte, string) {
 	fake.addMutex.RLock()
 	defer fake.addMutex.RUnlock()
-	return fake.addArgsForCall[i].operation
+	argsForCall := fake.addArgsForCall[i]
+	return argsForCall.arg1, argsForCall.arg2
 }
 
 func (fake *BatchWriter) AddReturns(result1 error) {
+	fake.addMutex.Lock()
+	defer fake.addMutex.Unlock()
 	fake.AddStub = nil
 	fake.addReturns = struct {
 		result1 error
@@ -75,6 +89,8 @@ func (fake *BatchWriter) AddReturns(result1 error) {
 }
 
 func (fake *BatchWriter) AddReturnsOnCall(i int, result1 error) {
+	fake.addMutex.Lock()
+	defer fake.addMutex.Unlock()
 	fake.AddStub = nil
 	if fake.addReturnsOnCall == nil {
 		fake.addReturnsOnCall = make(map[int]struct {
@@ -89,7 +105,8 @@ func (fake *BatchWriter) AddReturnsOnCall(i int, result1 error) {
 func (fake *BatchWriter) Start() error {
 	fake.startMutex.Lock()
 	ret, specificReturn := fake.startReturnsOnCall[len(fake.startArgsForCall)]
-	fake.startArgsForCall = append(fake.startArgsForCall, struct{}{})
+	fake.startArgsForCall = append(fake.startArgsForCall, struct {
+	}{})
 	fake.recordInvocation("Start", []interface{}{})
 	fake.startMutex.Unlock()
 	if fake.StartStub != nil {
@@ -98,7 +115,8 @@ func (fake *BatchWriter) Start() error {
 	if specificReturn {
 		return ret.result1
 	}
-	return fake.startReturns.result1
+	fakeReturns := fake.startReturns
+	return fakeReturns.result1
 }
 
 func (fake *BatchWriter) StartCallCount() int {
@@ -107,7 +125,15 @@ func (fake *BatchWriter) StartCallCount() int {
 	return len(fake.startArgsForCall)
 }
 
+func (fake *BatchWriter) StartCalls(stub func() error) {
+	fake.startMutex.Lock()
+	defer fake.startMutex.Unlock()
+	fake.StartStub = stub
+}
+
 func (fake *BatchWriter) StartReturns(result1 error) {
+	fake.startMutex.Lock()
+	defer fake.startMutex.Unlock()
 	fake.StartStub = nil
 	fake.startReturns = struct {
 		result1 error
@@ -115,6 +141,8 @@ func (fake *BatchWriter) StartReturns(result1 error) {
 }
 
 func (fake *BatchWriter) StartReturnsOnCall(i int, result1 error) {
+	fake.startMutex.Lock()
+	defer fake.startMutex.Unlock()
 	fake.StartStub = nil
 	if fake.startReturnsOnCall == nil {
 		fake.startReturnsOnCall = make(map[int]struct {
@@ -128,7 +156,8 @@ func (fake *BatchWriter) StartReturnsOnCall(i int, result1 error) {
 
 func (fake *BatchWriter) Stop() {
 	fake.stopMutex.Lock()
-	fake.stopArgsForCall = append(fake.stopArgsForCall, struct{}{})
+	fake.stopArgsForCall = append(fake.stopArgsForCall, struct {
+	}{})
 	fake.recordInvocation("Stop", []interface{}{})
 	fake.stopMutex.Unlock()
 	if fake.StopStub != nil {
@@ -140,6 +169,12 @@ func (fake *BatchWriter) StopCallCount() int {
 	fake.stopMutex.RLock()
 	defer fake.stopMutex.RUnlock()
 	return len(fake.stopArgsForCall)
+}
+
+func (fake *BatchWriter) StopCalls(stub func()) {
+	fake.stopMutex.Lock()
+	defer fake.stopMutex.Unlock()
+	fake.StopStub = stub
 }
 
 func (fake *BatchWriter) Invocations() map[string][][]interface{} {
